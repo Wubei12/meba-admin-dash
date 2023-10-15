83992d0f1492d3d7ea7c51f5875f06d441202ff8
Wubei 12 <wubshetyenguss@gmail.com>
2023-10-15 22:32:04 +0300

    register commit

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -6,7 +6,7 @@ import bgImg1 from '@/public/images/bg-landing1.jpeg'
 import bgImg2 from '@/public/images/bg-landing2.jpeg'
 import bgImg3 from '@/public/images/bg-landing3.jpeg'
 import bgImg4 from '@/public/images/bg-landing4.jpeg'
-import bgImg5 from '@/public/images/bg-landing5.jpg'
+import logoImg from '@/public/images/logo.png'
 import bgImg6 from '@/public/images/bg-landing6.jpeg'
 import Image from 'next/image';
 import { useRegisterMutation } from '@/redux/features/authApiSlice'
@@ -56,19 +56,19 @@ function Page() {
                 <div className='flex relative w-full h-full'>
                     <div className='absolute flex w-full h-full bg-black/50 z-10' />
                     <Image src={bgImg4} objectFit='contain' width={700} height={810} alt="" />
-                    <div className='absolute w-[6.5rem] h-screen justify-center shadow-2xl shadow-black z-10'></div>
+                    {/* <div className='absolute w-[6.5rem] h-screen justify-center shadow-2xl shadow-black z-10'></div> */}
                     <Image src={bgImg3} objectFit='contain' width={700} height={810} alt="" />
                     <Image src={bgImg6} objectFit='contain' width={700} height={810} alt="" />
                 </div>
             </div>
             <div className="flex absolute items-center bg-white z-20 lg:right-0 rounded-xl w-full max-w-sm md:max-w-2xl shadow-2xl md:mx-8 flex-1 flex-col justify-center md:py-10 md:my-0 my-4 py-10">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    {/* <img
-                        className="mx-auto h-10 w-auto"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                    <Image
+                        className="mx-auto h-24 w-auto"
+                        src={logoImg}
                         alt="Meba"
-                    /> */}
-                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                    />
+                    <h2 className="mt-10 text-center text-[#fdcd49] text-2xl font-bold leading-9 tracking-tight">
                         Sign up for an account
                     </h2>
                     <p className="mt-4 text-gray-500 text-center dark:text-gray-400">
