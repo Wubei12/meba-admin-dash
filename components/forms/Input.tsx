75336819117f7e113b9ab75d9489b6c5fa6c1ee9
Wubei 12 <wubshetyenguss@gmail.com>
2023-10-17 02:02:59 +0300

    Responsiveness

--- components/forms/Input.tsx
+++ components/forms/Input.tsx
@@ -33,7 +33,7 @@ function Input({ children, type, onChange, link, value, labelId, required = fals
             <div className="mt-2">
                 <input
                     id={labelId}
-                    className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#fdd35d] sm:text-sm sm:leading-6  transition duration-300"
+                    className="block w-full rounded-md border-0 py-1.5 sm:px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#fdd35d] sm:text-sm sm:leading-6  transition duration-300"
                     name={labelId}
                     type={type}
                     onChange={onChange}
